7c6aeba0e84c8e30266cd8da84a0b170940aed75
Ana Paula Moreira <aninhapaulinha94@hotmail.com>
2020-11-30 17:12:30 -0200

    exercicio-java

---
+++ semana3/Variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,22 @@
+/*10
+10,5*/
+
+/10 10 10/
+
+
+let nome
+let idade
+console.log(nome,idade)
+
+typeof nome,idade
+
+/* voltou as duas como undefined pois não foi acrescentado nenhum 
+valor nas variaveis*/
+
+let nome = "Ana Paula"
+console.log(nome)
+const idade= "28"
+console.log(idade)
+
+typeof nome
+typeof idade
