6694397f8dd0cf7966e957c9dc10b6d8057fd248
Ana Paula Moreira <aninhapaulinha94@hotmail.com>
2020-11-30 21:40:56 -0200

    variaveis

--- semana3/Variaveis/js-vanilla-template/src/index.js
+++ semana3/Variaveis/js-vanilla-template/src/index.js
@@ -1,22 +1,69 @@
-/*10
-10,5*/
+ Exerc 1:
+ 10
+10,5
 
-/10 10 10/
+Exerc 2:
+10 10 10
 
 
-let nome
-let idade
-console.log(nome,idade)
 
-typeof nome,idade
-
-/* voltou as duas como undefined pois não foi acrescentado nenhum 
+//Exercicio 1:
+ voltou as duas como undefined pois não foi acrescentado nenhum 
 valor nas variaveis*/
 
-let nome = "Ana Paula"
-console.log(nome)
-const idade= "28"
-console.log(idade)
+voltou as duas como string
 
-typeof nome
-typeof idade
+const nome = "Ola Ana Paula, voce tem" 
+let idade = "28 anos"
+console.log( nome, idade)
+
+//Exercicio 2:
+const endereço = "Qual seu endereço?"
+const resposta = "Resposta:Rua japin 332"
+console.log(endereço,resposta)
+
+let cor = "Qual sua cor preferida?"
+let resposta = "Resposta:Azul"
+console.log(cor,resposta)
+
+let comida = "Qual a sua comida preferida?"
+let resposta = "Resposta:Feijão e Arroz"
+console.log(comida,resposta)
+
+let cidades = "Em quais estados ja morou?"
+let resposta = "Resposta:Goias,Rio de Janeiro e Parana"
+console.log(cidades,resposta)
+
+let serie = "Qual a sua serie preferida?"
+let resposta = " Resposta:Greys Anatomy"
+console.log(serie,resposta)
+
+let comidas = ['lasanha','sushi','camarão','galinhada','macarrão']
+console.log(comidas)
+
+//Exercicio 3:
+let preferidas = [ 'Essas são minhas comidas preferidas:','lasanha','sushi','camarão','galinhada','macarrão']
+console.log(preferidas[0])
+console.log(preferidas[1])
+console.log(preferidas[2])
+console.log(preferidas[3])
+console.log(preferidas[4])
+console.log(preferidas[5])
+
+let preferidas = [ 'Qual sua comida favorita?','lasanha','Pizza','camarão','galinhada','macarrão']
+console.log(preferidas[0])
+console.log(preferidas[1])
+console.log(preferidas[2])
+console.log(preferidas[3])
+console.log(preferidas[4])
+console.log(preferidas[5])
+
+//Exercicio 4:
+
+let perguntas = ["Voce comeu hoje?","voce ja descansou hoje?", "Ja tomou banho ?"]
+let respostas = [true,false,true]
+console.log(perguntas[0],respostas[0])
+console.log(perguntas[1],respostas[1])
+console.log(perguntas[2],respostas[2])
+
+
