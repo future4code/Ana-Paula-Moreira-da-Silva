654198278a4acfef1f7a3eefe9a6dd2b514893e6
Ana Paula Moreira <aninhapaulinha94@hotmail.com>
2020-12-01 17:36:33 -0200

    java2

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,16 @@
+/*let idade1 = prompt("Qual a sua idade?")
+console.log(idade1)
+let idadeAmiga = prompt("Qual a idade da sua melhor amiga?")
+console.log(idadeAmiga)
+let resultado1 = (idade1 > idadeAmiga)
+console.log("Sua idade e maior que a da sua amiga?", resultado1)
+let resultado2 = (idade1 < idadeAmiga)
+console.log("Sua idade e maior que a da sua amiga?", resultado2)
+let resultado3 = Number(idade1-idadeAmiga)
+console.log("A diferença de idade sua e da sua amiga e de:", resultado3,"anos")
+
+28
+25
+Sua idade e maior que a da sua amiga? tru
+Sua idade e maior que a da sua amiga? false*/
+
