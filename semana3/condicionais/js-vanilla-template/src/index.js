b9d6c3e23c3442896231c2c839b26fe3be540923
Ana Paula Moreira <aninhapaulinha94@hotmail.com>
2020-12-02 17:51:48 -0200

    java-02

---
+++ semana3/condicionais/js-vanilla-template/src/index.js
@@ -0,0 +1,29 @@
+
+/*EXERCICIO2
+
+
+a)Para determinar o valor da fruta indicada
+b)O preço da fruta, maçã, é R$ 2,25
+c)O preço da fruta  Pêra  é  R$  5.5*/
+
+/*EXERCICIO3
+
+a)Esta fazenda uma pergunda ao usuario, em mumero
+b)Esse número passou no teste// da erro informando 
+mensagem não está definida
+c)Da erro, pelo motivo */
+
+/*EXERCÍCIO 4
+
+const idade = Number(prompt("Quantos anos voce tem?"))
+const maior = Number(18)
+if(idade >=18 ){
+console.log("Ela pode dirigir")
+}else{
+    console.log("Ela nao pode dirigir")
+}*/
+
+
+
+
+
---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,9 @@
+const respostaDoUsuario = prompt("Digite o número que você quer testar")
+const numero = Number(respostaDoUsuario)
+
+if(numero % 2 === 0) {
+  console.log("Passou no teste.")
+} else {
+  console.log("Não passou no teste.")
+}
+
