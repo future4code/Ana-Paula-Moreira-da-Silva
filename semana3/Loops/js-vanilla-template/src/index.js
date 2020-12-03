9e74a202b72ad837f0f93c3d9e8e5c076cdd509d
Ana Paula Moreira <aninhapaulinha94@hotmail.com>
2020-12-03 17:34:16 -0200

    java3

---
+++ semana3/Loops/js-vanilla-template/src/index.js
@@ -0,0 +1,40 @@
+/*const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}*/
+
+/*EXERCÍCIO 3
+a)
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[3])
+console.log(array[4])
+console.log(array[5])
+console.log(array[6])
+console.log(array[7])
+console.log(array[8])
+console.log(array[9])
+console.log(array[10])
+console.log(array[11])*/
+
+
+/*b)
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+let dividir = 10
+console.log(array[0]/dividir)
+console.log(array[1]/dividir)
+console.log(array[2]/dividir)
+console.log(array[3]/dividir)
+console.log(array[4]/dividir)
+console.log(array[5]/dividir)
+console.log(array[6]/dividir)
+console.log(array[7]/dividir)
+console.log(array[8]/dividir)
+console.log(array[9]/dividir)
+console.log(array[10]/dividir)
+console.log(array[11]/dividir)*/
+
