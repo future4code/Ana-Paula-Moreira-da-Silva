c71405e80817f98c22468436a81ffdf1e92c8067
Ana Paula Moreira <aninhapaulinha94@hotmail.com>
2020-12-03 17:37:36 -0200

    java3

--- semana3/Loops/js-vanilla-template/src/index.js
+++ semana3/Loops/js-vanilla-template/src/index.js
@@ -1,9 +1,15 @@
-/*const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
-for (let numero of lista) {
-  if (numero > 18) {
-		console.log(numero)
-	}
-}*/
+/*EXERCÍCIO 1
+O que o código abaixo está fazendo? Qual o resultado impresso no console?
+Esta somando , o resultado e 10
+
+EXERCÍCIO 2
+a. O que vai ser impresso no console?
+19
+21
+23
+25
+27
+30*/
 
 /*EXERCÍCIO 3
 a)
