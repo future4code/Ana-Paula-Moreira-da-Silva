6ca818a018307828aa0d0e97c04a568be203ed61
Ana Paula Moreira <aninhapaulinha94@hotmail.com>
2020-12-06 23:22:42 -0200

    jogo

--- semana3/Blackjack/nossoJogo.js
+++ semana3/Blackjack/nossoJogo.js
@@ -10,3 +10,22 @@
  * 
  * 
  */
+
+console.log("Bem vindo ao jogo de Blackack")
+
+
+if(confirm("pergunta de sim ou não")) {
+   // o que fazer se o usuário clicar "ok"
+   const cartaUsuarioA = comprarCarta()
+   const cartaUsuarioB = comprarCarta()
+   const cartaPc1 = comprarCarta()
+   const cartaPc2 = comprarCarta()
+   const somaUsuario = cartaUsuarioA.valor + cartaUsuarioB.valor
+   const somaPc = cartaPc1.valor + cartaPc2.valor
+
+   console.log('Usuario - cartas: ${cartaUsuarioA.texto} ${cartaUsuarioB.texto} - pontuação = ${somaUsuario}') 
+   console.log('computador - cartas: ${cartaPc1.texto} ${cartaPc2.texto} - pontuação = ${somaPc}')
+} else {
+   // o que fazer se o usuário clicar "cancelar"
+   console.log("o jogo acabou")
+}
