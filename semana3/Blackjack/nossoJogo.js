ee6b084663a2deef427304cd8379f2f14dd60d5e
Ana Paula Moreira <aninhapaulinha94@hotmail.com>
2020-12-07 15:44:40 -0200

    atualização

--- semana3/Blackjack/nossoJogo.js
+++ semana3/Blackjack/nossoJogo.js
@@ -10,22 +10,12 @@
  * 
  * 
  */
+console.log("oi!")
 
-console.log("Bem vindo ao jogo de Blackack")
-
-
-if(confirm("pergunta de sim ou não")) {
-   // o que fazer se o usuário clicar "ok"
-   const cartaUsuarioA = comprarCarta()
-   const cartaUsuarioB = comprarCarta()
-   const cartaPc1 = comprarCarta()
-   const cartaPc2 = comprarCarta()
-   const somaUsuario = cartaUsuarioA.valor + cartaUsuarioB.valor
-   const somaPc = cartaPc1.valor + cartaPc2.valor
-
-   console.log('Usuario - cartas: ${cartaUsuarioA.texto} ${cartaUsuarioB.texto} - pontuação = ${somaUsuario}') 
-   console.log('computador - cartas: ${cartaPc1.texto} ${cartaPc2.texto} - pontuação = ${somaPc}')
-} else {
-   // o que fazer se o usuário clicar "cancelar"
-   console.log("o jogo acabou")
+function minhafuncao(quantidade) {
+   for (let i = l; i <= quantidade; i++) {
+      console.log(i*2)
+   }
 }
+
+minhafuncao(5)
