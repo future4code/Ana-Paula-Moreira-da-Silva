88154fef3c328698b18381836704be92e4976b7f
Ana Paula Moreira <aninhapaulinha94@hotmail.com>
2020-12-11 17:50:46 -0200

    interpretação-de-codigo

--- semana4/funcoes/js-vanilla-template/src/index.js
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -53,3 +53,49 @@ soma1(primeiro, segundo, terceiro, quarto)*/
  const arrayDeNumero = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
  const quantidade2 = meuArray(arrayDeNumero)
   console.log("quantidade: ", quantidade2)*/
+
+
+  /*function conversorDeMoeda(valorEmDolar){
+      const cotacao = Number(prompt("Informe o valor da cotação do dolar"));
+
+      return "R$" + (valorEmDolar* cotacao);
+
+  }
+
+  const meuDinheiro = conversorDeMoeda(100);
+  
+  console.log(meuDinheiro);
+
+  const numeros = [25,12,55,64,121,44,11,84,51,48,14,73,111,283];
+  const array1 = [];
+  const array2 = [];
+
+  for(let numero of numeros){
+      if(numero % 2 === 0){
+          array1.push(numero);
+      } else {
+          array2.push(numero);
+      }
+  }
+
+  console.log("Quantidade total de numeros", numeros.length)
+  console.log(array1.length)
+  console.log(array2.length)*/
+
+  const numeros = [25,12,55,64,121,44,11,84,51,48,14,73,111,283,1,99,13,31,83,131,1,1.1,-10,25,1590];
+  
+  let numero1 = Infinity;
+  let numero2 = 0;
+
+  for(let numero of numeros){
+      if(numero < mumero1){
+          numero1 = numero;
+      }
+      
+      if(numero > numero2){
+          numero2 = numero;
+      }
+  }
+
+  console.log(numero1);
+  console.log(numero2);
---
+++ semana4/lista-de-exercicios/lista.js
@@ -0,0 +1,20 @@
+/*Analise os trechos de código em cada exercício e escreva o que ele faz, 
+como faz e qual será o valor impresso no `console`.
+1= o exemplo me retorna o valor da conversão do valor de acordo com a cotação
+coloca de R$ 5,08.
+
+2= Ele vai imprimir a mensagem de alerta informado, 
+caso não tivessemos a função alert, seria imprimido somente o 
+novo montante, porque no segundo montante, tem o tesouro direto
+que não foi declarado acima, então setia undefined.
+
+3= o codigo volta a quantidade de numeros no array,
+a quantidade de numeros dividos por 2, no caso par
+e a quantidade de numeros que não são inteiros, impar 
+Ela declara o array principal, usa o push para puxar as informações 
+de divisão de sim ou não, e usa o length que retorna a "quantidade" pedida
+
+4= a variavel numero 1 e infinto que começa no 0, e a numero 2 e 0, se o numero da array for menor
+que o infinito, ou seja o menor numero que tive na array ele vai recebe -10, e a outta
+diz que se o numero da array for maior que zero vai ser recebido 283 que e o que esta 
+na array*/
