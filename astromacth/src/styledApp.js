3ea96eda5f92f2011d96fccb7a9bb927b53a004f
Ana Paula Moreira <aninhapaulinha94@hotmail.com>
2021-01-29 19:01:22 -0300

    criação

---
+++ astromacth/src/styledApp.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const AppConteiner = styled.div`
+   background-color: black;
+   height: 100vh;
+   width: 100vw;
+   margin: 0;
+   position: absolute;
+`
+// export const Break = styled.hr`
+//    width: 95%;
+//    height: 1px;
+//    background: linear-gradient(to right, #E783FD, #83D5FF);
+//    margin-bottom: 4%; 
+// `
