af0415b015fab70072c40aa88060158487cb15cb
Ana-moreira <anabrtcc88@gmail.com>
2021-03-15 16:27:59 -0300

    Node, com pasta separada

---
+++ semana15/exercicio1.js
@@ -0,0 +1,7 @@
+const idade = Number(process.argv[3])
+console.log(`Olá, ${process.argv[2]}! Voce tem ${idade} anos.`);
+
+const idade = Number(process.argv[3])
+console.log(`Olá, ${process.argv[2]}! Voce tem ${idade} anos. Em sete anos voce tera ${process.argv[4]}`)
+
+
